bc2da9bb9dd26f3cfafe34376f3aaa4227e9b8f5
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-08-25 14:56:03 -0700

    page-layout-and-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {HomeModule} from './home/home.module';
+
 import {
-  SharedModule
+  SharedModule,
+  FooterComponent,
+  HeaderComponent
 } from './shared';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true })
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    HomeModule,
+    rootRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
