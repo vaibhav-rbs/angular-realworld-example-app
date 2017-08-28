c71188f81e63251fd787bdaceeaead1275c3b26a
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-08-27 21:13:03 -0700

    WIP commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-page',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  constructor () {}
+  authForm: FormGroup;
+
+
+  constructor (
+    private fb: FormBuilder
+  ) {
+    this.authForm = this.fb.group({
+      'email': [' ', Validators.required],
+      'password': [' ', Validators.required ]
+    })
+  }
+
+  submitForm(){
+    let credentials = this.authForm.value;
+    console.log(credentials);
+  }
 
 }
