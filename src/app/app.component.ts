4e27ab4726b576d4c6f8f465bb2f0ef17b44ff3d
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-08-27 22:32:38 -0700

    correcting my sins.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-page',
+  selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  authForm: FormGroup;
-
-
-  constructor (
-    private fb: FormBuilder
-  ) {
-    this.authForm = this.fb.group({
-      'email': [' ', Validators.required],
-      'password': [' ', Validators.required ]
-    })
-  }
-
-  submitForm(){
-    let credentials = this.authForm.value;
-    console.log(credentials);
-  }
+  constructor () {}
 
 }
